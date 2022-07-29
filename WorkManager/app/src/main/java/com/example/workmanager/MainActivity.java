dcd0f05b5dc7bda01b03dd5347729e4d88f57f35
ruhiuzunr <garfield41414@gmail.com>
2022-07-29 18:28:58 +0300

    Work Manager ile telefon içi bildirim gönderme.
    Çalışma kodları, Kurulumu, Zaman bildirim göstergesi.

--- WorkManager/app/src/main/java/com/example/workmanager/MainActivity.java
+++ WorkManager/app/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.workmanager.databinding.ActivityMainBinding;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding tasarim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(tasarim.getRoot());
+
+        tasarim.buttonYap.setOnClickListener(view -> {/*
+
+            //Şartımız hazır
+            Constraints calismaKosulu = new Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .build();
+            //İsteğimiz
+            WorkRequest istek = new OneTimeWorkRequest.Builder(MyWorker.class)
+                    .setInitialDelay(10, TimeUnit.SECONDS)
+                    .setConstraints(calismaKosulu)
+                    .build();
+
+            WorkManager.getInstance(this).enqueue(istek);
+
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(istek.getId())
+                    .observe(this,workInfo -> {
+                        String Durum = workInfo.getState().name();
+                        Log.e("Arkaplan işlem",Durum);
+                    });
+                    */
+
+            WorkRequest istek = new PeriodicWorkRequest
+                    .Builder(MyWorkerBildirim.class,1,TimeUnit.MINUTES)
+                    .setInitialDelay(5,TimeUnit.SECONDS)
+                    .build();
+            WorkManager.getInstance(this).enqueue(istek);
+        });
+
     }
 }
